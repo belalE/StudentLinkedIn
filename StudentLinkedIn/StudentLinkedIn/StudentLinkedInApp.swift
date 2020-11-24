e48b8f630109bd84017530c939f1dab48ff63243
belalE <balolyman1234@gmail.com>
2020-11-24 14:27:50 -0600

    initial commit

---
+++ StudentLinkedIn/StudentLinkedIn/StudentLinkedInApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudentLinkedInApp.swift
+//  StudentLinkedIn
+//
+//  Created by Belal Elsiesy on 11/24/20.
+//
+
+import SwiftUI
+
+@main
+struct StudentLinkedInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
